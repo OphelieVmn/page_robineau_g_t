7dabe96d717e5656925208ad85977cdf5d2fddd9
tarik <anvaralytarik325@gmail.com>
2022-06-05 18:00:39 +0200

    java contact form

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,15 +1,47 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>Envoi d'un message par formulaire</title>
-</head>
+<?php 
+    require_once(__DIR__ . '/vendor/autoload.php');
+    use \Mailjet\Resources;
+    define('API_PUBLIC_KEY', 'YOUR_KEY');
+    define('API_PRIVATE_KEY', 'YOUR_KEY');
+    $mj = new \Mailjet\Client(API_PUBLIC_KEY, API_PRIVATE_KEY,true,['version' => 'v3.1']);
 
-<body>
-    <?php
-    $retour = mail('robineau_gt@yopmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@robineau.com');
-    if ($retour)
-        echo '<p>Votre message a bien été envoyé.</p>';
-    ?>
-</body>
-</html>
+
+    if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['message'])){
+        $surname = htmlspecialchars($_POST['surname']);
+        $firstname = htmlspecialchars($_POST['firstname']);
+        $email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $body = [
+            'Messages' => [
+            [
+                'From' => [
+                'Email' => "EMAIL",
+                'Name' => "NAME"
+                ],
+                'To' => [
+                [
+                    'Email' => "EMAIL",
+                    'Name' => "NAME"
+                ]
+                ],
+                'Subject' => "SUJECT",
+                'TextPart' => 'TEXT EMAIL', 
+                'HTMLPart' => "TEXT EMAIL",
+                'CustomID' => "AppGettingStartedTest"
+            ]
+            ]
+        ];
+            $response = $mj->post(Resources::$Email, ['body' => $body]);
+            $response->success();
+            echo "Email envoyé avec succès !";
+        }
+        else{
+            echo "Email non valide";
+        }
+
+    } else {
+        header('Location: index.php');
+        die();
+    }
